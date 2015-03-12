450e4f3e40d6550e9a335b25fd4b0f61fd551acf
Rania NEJEH <rania@Mac-mini-de-Yiming.local>
2015-03-12 11:02:56 +0100

    ajout des fichiers du prjt

---
+++ TestSourceTree_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSourceTree
+//
+//  Created by Rania NEJEH  on 12/03/2015.
+//  Copyright (c) 2015 Airweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSourceTree_/ViewController - copie.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestSourceTree
+//
+//  Created by Rania NEJEH  on 12/03/2015.
+//  Copyright (c) 2015 Airweb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+  ezfyeoyo
+@end   
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
